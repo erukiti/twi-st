3de7e651344cec9e8a78356c9da58ec9b0dd01f3
SASAKI, Shunsuke <erukiti  gmail  com>
2012-03-02 09:59:42 +0900

    uncaughtException イベントハンドラを登録する事でエラー対策

--- app.js
+++ app.js
@@ -42,3 +42,5 @@ console.log("Express server listening on port %d in %s mode", app.address().port
 global.io = socketio.listen(app);
 
 //search("ラーメン");
+
+process.on('uncaughtException', function(){console.debug()});
